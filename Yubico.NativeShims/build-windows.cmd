b8666328c119ff6095db04de153a0a6bb65fa93d
Greg Domzalski <greg@yubico.com>
2022-07-28 14:43:48 -0700

    Fix VCPKG triplets to match build architecture

--- Yubico.NativeShims/build-windows.cmd
+++ Yubico.NativeShims/build-windows.cmd
@@ -1,5 +1,5 @@
 :: 32-bit builds
-cmake -S . -B build32 -A Win32 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
+cmake -S . -B build32 -A Win32 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows-static
 cmake --build build32 --config Release
 mkdir win-x86
 copy build32\Release\Yubico.NativeShims.dll win-x86
@@ -11,7 +11,7 @@ mkdir win-x64
 copy build64\Release\Yubico.NativeShims.dll win-x64
 
 :: ARM64 builds
-cmake -S . -B buildarm -A arm64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
+cmake -S . -B buildarm -A arm64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_INSTALLATION_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=arm64-windows-static
 cmake --build buildarm --config Release
 mkdir win-arm64
 copy buildarm\Release\Yubico.NativeShims.dll win-arm64

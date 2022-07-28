9f15a43f1d1fab0293f02ca9f1845d728833d732
Greg Domzalski <greg@yubico.com>
2022-07-28 11:08:11 -0700

    Modify windows build for vcpkg usage

--- Yubico.NativeShims/build-windows.cmd
+++ Yubico.NativeShims/build-windows.cmd
@@ -1,17 +1,17 @@
 :: 32-bit builds
-cmake -S . -B build32 -A Win32
+cmake -S . -B build32 -A Win32 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build build32 --config Release
 mkdir win-x86
 copy build32\Release\Yubico.NativeShims.dll win-x86
 
 :: 64-bit builds
-cmake -S . -B build64 -A x64
+cmake -S . -B build64 -A x64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build build64 --config Release
 mkdir win-x64
 copy build64\Release\Yubico.NativeShims.dll win-x64
 
 :: ARM64 builds
-cmake -S . -B buildarm -A arm64
+cmake -S . -B buildarm -A arm64 -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build buildarm --config Release
 mkdir win-arm64
 copy buildarm\Release\Yubico.NativeShims.dll win-arm64

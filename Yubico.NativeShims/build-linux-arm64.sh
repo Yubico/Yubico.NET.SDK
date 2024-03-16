262599ee49634eb79826e28a67f33e46a56ff089
Dennis Dyall <dennis.dyall@yubico.com>
2024-03-16 01:48:17 +0100

    builld works but my wsl crashed and restarted

--- Yubico.NativeShims/build-linux-arm64.sh
+++ Yubico.NativeShims/build-linux-arm64.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# cd ~/ && cp -r /mnt/c/Users/Dennis.Dyall/Documents/Work/Yubico.NET.SDK-private/Yubico.NativeShims/ . 
-# echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
-# echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
+cd ~/ && cp -r /mnt/c/Users/Dennis.Dyall/Documents/Work/Yubico.NET.SDK-private/Yubico.NativeShims/ . 
+echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
+echo 'set completion-ignore-case On' | sudo tee -a /etc/inputrc
 
-#set -e
+# set -e
 
-export VCPKG_INSTALLATION_ROOT=$GITHUB_WORKSPACE/vcpkg \
+export VCPKG_INSTALLATION_ROOT=~/vcpkg \
     VCPKG_FORCE_SYSTEM_BINARIES=1 \
-    PATH=/usr/local/bin:$PATH
+    PATH=/usr/local/bin:$VCPKG_INSTALLATION_ROOT:$PATH
 
 sudo apt-get update -qq && \
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq \
@@ -28,43 +28,54 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq \
     g++-aarch64-linux-gnu \
     gcc-aarch64-linux-gnu
 
-echo "Installing latest Cmake"
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
-sudo apt-get update -qq && sudo apt-get install cmake -yq
+sudo apt-get update -qq
+sudo apt-get install cmake -yq
 
-echo "Installing VCPKG"
 git clone https://github.com/Microsoft/vcpkg.git ${VCPKG_INSTALLATION_ROOT} && ${VCPKG_INSTALLATION_ROOT}/bootstrap-vcpkg.sh
+echo "vcpkg installed!!" && vcpkg --version
 
+# Is this needed? Yes to install it. Unless we can find it from another source
 echo "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal main restricted universe multiverse
 deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal-updates main restricted universe multiverse
 deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal-security main restricted universe multiverse
-deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal-backports main restricted universe multiverse" | sudo tee /etc/apt/sources.list> /dev/null
-
-sudo apt-get update -qq | DEBIAN_FRONTEND=noninteractive sudo apt-get install libpcsclite-dev:arm64 -yq
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ focal-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list > /dev/null
+sudo dpkg --add-architecture arm64
+sudo apt-get update -qq
+sudo apt-get install libpcsclite-dev:arm64 -yq
 
 build_target() {
     local target_triplet=$1
-    build_dir="build-$target_triplet"
+    local build_dir=$2
+
+    # export OPENSSL_ROOT_DIR=$(pwd)/linux-arm64/vcpkg_installed/arm64-linux
+
+    echo "SSL DIR: $OPENSSL_ROOT_DIR"
 
     rm -rf "$build_dir"
     mkdir -p "$build_dir"
 
+    export PKG_CONFIG_PATH="/usr/lib/aarch64-linux-gnu/pkgconfig:$(pwd)/${target_triplet}/vcpkg_installed/arm64-linux/lib/pkgconfig"
+
     echo "Building for $target_triplet ..."
-    PKG_CONFIG_PATH="/usr/lib/aarch64-linux-gnu/pkgconfig:$(pwd)/${target_triplet}/vcpkg_installed/arm64-linux/lib/pkgconfig" cmake -S . -B "$build_dir" \
+    echo "PKG_CONFIG_PATH DIR: $PKG_CONFIG_PATH"
+
+    cmake -S . -B "$build_dir" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_TOOLCHAIN_FILE="$VCPKG_INSTALLATION_ROOT/scripts/buildsystems/vcpkg.cmake" \
         -DVCPKG_TARGET_TRIPLET="$target_triplet" \
         -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE="$(pwd)/cmake/aarch64-linux-gnu.toolchain.cmake" \
         -DOPENSSL_ROOT_DIR=$(pwd)/linux-arm64/vcpkg_installed/arm64-linux
 
+    
     cmake --build "$build_dir" -- -j $(nproc)
+    #cp "$build_dir"/*.so ./"$build_dir"
 }
 
 if [ ! -f ./CMakeLists.txt ]; then
     cd ~/Yubico.NativeShims
 fi
 
-build_target arm64-linux
-cp build-arm64-linux/*.so ./linux-arm64
+build_target arm64-linux linux-arm64
 

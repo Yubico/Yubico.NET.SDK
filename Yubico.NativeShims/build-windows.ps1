4427c8d5449c97bc5e13cd71d396a64976c16a2f
Dennis Dyall <dennis.dyall@yubico.com>
2024-06-28 22:25:50 +0200

    Optimize windows build

--- Yubico.NativeShims/build-windows.ps1
+++ Yubico.NativeShims/build-windows.ps1
@@ -7,19 +7,19 @@ vcpkg x-update-baseline
 Pop-Location
 
 # 32-bit builds
-cmake -S . -B build32 -A Win32 -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_INSTALLATION_ROOT\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static
+cmake -S . -B build32 -A Win32 -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_INSTALLATION_ROOT\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x86-windows-static -DCMAKE_BUILD_TYPE=Release -DOPENSSL_NO_DEBUG=ON -DOPENSSL_NO_TESTS=ON -DOPENSSL_NO_DOCS=ON
 cmake --build build32 --config Release
 mkdir win-x86
 Copy-Item build32\Release\Yubico.NativeShims.dll win-x86
 
 # 64-bit builds
-cmake -S . -B build64 -A x64 -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_INSTALLATION_ROOT\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static
+cmake -S . -B build64 -A x64 -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_INSTALLATION_ROOT\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Release -DOPENSSL_NO_DEBUG=ON -DOPENSSL_NO_TESTS=ON -DOPENSSL_NO_DOCS=ON
 cmake --build build64 --config Release
 mkdir win-x64
 Copy-Item build64\Release\Yubico.NativeShims.dll win-x64
 
 # ARM64 builds
-cmake -S . -B buildarm -A arm64 -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_INSTALLATION_ROOT\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=arm64-windows-static
+cmake -S . -B buildarm -A arm64 -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_INSTALLATION_ROOT\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=arm64-windows-static -DCMAKE_BUILD_TYPE=Release -DOPENSSL_NO_DEBUG=ON -DOPENSSL_NO_TESTS=ON -DOPENSSL_NO_DOCS=ON
 cmake --build buildarm --config Release
 mkdir win-arm64
 Copy-Item buildarm\Release\Yubico.NativeShims.dll win-arm64

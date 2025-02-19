de74c5a9dcdb972171a8f741536ebabd5353326b
Dennis Dyall <dennis.dyall@yubico.com>
2025-02-19 21:03:03 +0100

    fix: field _readerName cannot be auto property

--- Yubico.Core/src/Yubico/PlatformInterop/Desktop/SCard/SCardReaderStates.cs
+++ Yubico.Core/src/Yubico/PlatformInterop/Desktop/SCard/SCardReaderStates.cs
@@ -23,24 +23,30 @@ namespace Yubico.PlatformInterop
 {
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
     // Justification: Fields are read/write via interop. Readonly might not have any effect there, but it may give
-    // maintainers a false impression about the true nature of these fields.
+    // maintainers a falls impression about the true nature of these fields.
     [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
     [SuppressMessage("Style", "IDE0044:Add readonly modifier")]
     internal struct SCARD_READER_STATE
     {
+#pragma warning disable IDE0032 // Use auto property, leave as is for now, as it auto property may not work with interop
+        [MarshalAs(UnmanagedType.LPStr)]
+        private string _readerName;
+#pragma warning restore IDE0032 // Use auto property, leave as is for now, as it auto property may not work with interop
         private IntPtr _userData;
         private uint _currentState;
         private uint _eventState;
         private uint _atrLength;
-
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 36)]
         private byte[] _answerToReset;
 
         private const uint SequenceMask = 0xFFFF_0000;
         private const uint StateMask = 0x0000_FFFF;
 
-        [field: MarshalAs(UnmanagedType.LPStr)]
-        public string ReaderName { get; set; }
+        public string ReaderName
+        {
+            get => _readerName;
+            set => _readerName = value;
+        }
 
         public SCARD_STATE CurrentState => (SCARD_STATE)(_currentState & StateMask);
         public SCARD_STATE EventState => (SCARD_STATE)(_eventState & StateMask);

e1c539c68496296f4f0b3bdb05815ab81452a02b
Dennis Dyallo <dennis.dyall@yubico.com>
2025-04-03 11:45:28 +0200

    refactor: use tlvwriter

--- Yubico.YubiKey/src/Yubico/YubiKey/Piv/KeyParametersPivExtensions.cs
+++ Yubico.YubiKey/src/Yubico/YubiKey/Piv/KeyParametersPivExtensions.cs
@@ -47,16 +47,14 @@ public static class KeyParametersPivExtensions
     private static Memory<byte> EncodeRSAPublicKeyParameters(RSAPublicKeyParameters parameters)
     {
         var rsaParameters = parameters.Parameters;
-        var tlvs = new Dictionary<int, byte[]>
+        var tlvWriter = new TlvWriter();
+        using (tlvWriter.WriteNestedTlv(PivConstants.PublicKeyTag))
         {
-            { PivConstants.PublicRSAModulusTag, rsaParameters.Modulus },
-            { PivConstants.PublicRSAExponentTag, rsaParameters.Exponent }
-        };
+            tlvWriter.WriteValue(PivConstants.PublicRSAModulusTag, rsaParameters.Modulus);
+            tlvWriter.WriteValue(PivConstants.PublicRSAExponentTag, rsaParameters.Exponent);
+        }
 
-        return new TlvObject(
-                PivConstants.PublicKeyTag,
-                TlvObjects.EncodeDictionary(tlvs).Span)
-            .GetBytes();
+        return tlvWriter.Encode();
     }
 
     private static Memory<byte> EncodeCurve25519PublicKeyParameters(Curve25519PublicKeyParameters parameters) =>
@@ -67,25 +65,14 @@ public static class KeyParametersPivExtensions
 
     private static Memory<byte> EncodeECPrivateKeyParameters(ECPrivateKeyParameters parameters)
     {
-        // byte[] tlv = new byte[1 + parameters.PrivateKey.Length];
-        // tlv[0] = PivConstants.PrivateECDsaTag;
-        // tlv[1] = (byte)parameters.PrivateKey.Length;
-        // parameters.PrivateKey.CopyTo(tlv.AsMemory()[2..]);
-        // return tlv;
-
         var tlvWriter = new TlvWriter();
         tlvWriter.WriteValue(PivConstants.PrivateECDsaTag, parameters.PrivateKey.Span);
         return tlvWriter.Encode();
-
-        // var tlvObject = new TlvObject(PivConstants.PrivateECDsaTag, parameters.PrivateKey.Span);
-        // return tlvObject.GetBytes();
     }
 
     private static Memory<byte> EncodeRSAPrivateKeyParameters(RSAPrivateKeyParameters parameters)
     {
         var rsaParameters = parameters.Parameters;
-
-        // Verify all RSA parameters are of correct length
         if (rsaParameters.P.Length != rsaParameters.Q.Length ||
             rsaParameters.DP.Length != rsaParameters.P.Length ||
             rsaParameters.DQ.Length != rsaParameters.P.Length ||
@@ -97,41 +84,23 @@ public static class KeyParametersPivExtensions
                     ExceptionMessages.InvalidPrivateKeyData));
         }
 
-        // var tlvWriter = new TlvWriter();
-        // tlvWriter.WriteValue(PivConstants.PrivateRSAPrimePTag, parameters.P);
-        // tlvWriter.WriteValue(PivConstants.PrivateRSAPrimeQTag, parameters.Q);
-        // tlvWriter.WriteValue(PivConstants.PrivateRSAExponentPTag, parameters.DP);
-        // tlvWriter.WriteValue(PivConstants.PrivateRSAExponentQTag, parameters.DQ);
-        // tlvWriter.WriteValue(PivConstants.PrivateRSACoefficientTag, parameters.InverseQ);
-        // return tlvWriter.Encode();
-
-        var tlvs = new Dictionary<int, byte[]>
-        {
-            { PivConstants.PrivateRSAPrimePTag, rsaParameters.P },
-            { PivConstants.PrivateRSAPrimeQTag, rsaParameters.Q },
-            { PivConstants.PrivateRSAExponentPTag, rsaParameters.DP },
-            { PivConstants.PrivateRSAExponentQTag, rsaParameters.DQ },
-            { PivConstants.PrivateRSACoefficientTag, rsaParameters.InverseQ }
-        };
-
-        return TlvObjects.EncodeDictionary(tlvs);
+        var tlvWriter = new TlvWriter();
+        tlvWriter.WriteValue(PivConstants.PrivateRSAPrimePTag, rsaParameters.P);
+        tlvWriter.WriteValue(PivConstants.PrivateRSAPrimeQTag, rsaParameters.Q);
+        tlvWriter.WriteValue(PivConstants.PrivateRSAExponentPTag, rsaParameters.DP);
+        tlvWriter.WriteValue(PivConstants.PrivateRSAExponentQTag, rsaParameters.DQ);
+        tlvWriter.WriteValue(PivConstants.PrivateRSACoefficientTag, rsaParameters.InverseQ);
+        return tlvWriter.Encode();
     }
 
     private static Memory<byte> EncodeCurve25519PrivateKeyParameters(Curve25519PrivateKeyParameters parameters)
     {
-        // byte[] tlv = new byte[1 + parameters.PrivateKey.Length];
-        // tlv[0] = (byte)(parameters.KeyType == KeyType.Ed25519 ? PivConstants.PrivateECEd25519Tag : PivConstants.PrivateECX25519Tag);
-        // tlv[1] = (byte)parameters.PrivateKey.Length;
-        // parameters.PrivateKey.CopyTo(tlv.AsMemory()[2..]);
-        // return tlv;
-
         var tlvWriter = new TlvWriter();
         int typeTag = parameters.KeyType == KeyType.Ed25519
             ? PivConstants.PrivateECEd25519Tag
             : PivConstants.PrivateECX25519Tag;
 
         tlvWriter.WriteValue(typeTag, parameters.PrivateKey.Span);
-
         return tlvWriter.Encode();
     }
 }

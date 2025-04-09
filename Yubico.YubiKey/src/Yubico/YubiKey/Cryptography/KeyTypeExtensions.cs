a5bb31e2dcd781c4ccb74f80b7308ef8687dfbed
Dennis Dyallo <dennis.dyall@yubico.com>
2025-04-09 14:19:18 +0200

    feat: add various extensions to KeyType

--- Yubico.YubiKey/src/Yubico/YubiKey/Cryptography/KeyTypeExtensions.cs
+++ Yubico.YubiKey/src/Yubico/YubiKey/Cryptography/KeyTypeExtensions.cs
@@ -16,9 +16,11 @@ namespace Yubico.YubiKey.Cryptography;
 
 public static class KeyTypeExtensions
 {
-    public static string ToAlgorithmOid(this KeyType keyType) => Oids.GetOidsByKeyType(keyType).AlgorithmOid;    
-    public static string? ToCurveOid(this KeyType keyType) => Oids.GetOidsByKeyType(keyType).Curveoid;    
     public static KeyDefinition GetKeyDefinition(this KeyType keyType) => KeyDefinitions.GetByKeyType(keyType);
+    public static string GetAlgorithmOid(this KeyType keyType) => Oids.GetOidsByKeyType(keyType).AlgorithmOid;    
+    public static string? GetCurveOid(this KeyType keyType) => Oids.GetOidsByKeyType(keyType).Curveoid;    
+    public static int GetKeySizeBits(this KeyType keyType) => keyType.GetKeyDefinition().LengthInBits;
+    public static int GetKeySizeBytes(this KeyType keyType) => keyType.GetKeyDefinition().LengthInBytes;
     public static bool IsCoseKey(this KeyType keyType) => keyType.GetKeyDefinition().CoseKeyDefinition != null;
     public static bool IsECDsa(this KeyType keyType) => keyType is KeyType.ECP256 or KeyType.ECP384 or KeyType.ECP521;
     public static bool IsCurve25519(this KeyType keyType) => keyType is KeyType.X25519 or KeyType.Ed25519;
@@ -26,10 +28,8 @@ public static class KeyTypeExtensions
         IsEllipticCurve(keyType) || IsRSA(keyType);
     public static bool IsSymmetric(this KeyType keyType) => 
         keyType is KeyType.TripleDES or KeyType.AES128 or KeyType.AES192 or KeyType.AES256;
-    
     public static bool IsEllipticCurve(this KeyType keyType) => 
         keyType is KeyType.ECP256 or KeyType.ECP384 or KeyType.ECP521 or KeyType.X25519 or KeyType.Ed25519;
-    
     public static bool IsRSA(this KeyType keyType) =>
         keyType is KeyType.RSA1024 or KeyType.RSA2048 or KeyType.RSA3072 or KeyType.RSA4096;
 }

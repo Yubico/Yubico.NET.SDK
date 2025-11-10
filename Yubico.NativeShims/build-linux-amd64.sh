b6bd531a748b53ea47ddba024475eb1b6d1382c5
Dennis Dyallo <dennis.dyall@yubico.com>
2025-11-10 16:25:20 +0100

    build: update CMake

--- Yubico.NativeShims/build-linux-amd64.sh
+++ Yubico.NativeShims/build-linux-amd64.sh
@@ -28,9 +28,9 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -yq \
     g++ \
     gcc
 
-# Install latest version of CMake for Ubuntu 20.04
+# Install latest version of CMake for Ubuntu
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
-echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ noble main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update -qq
 sudo apt-get install cmake -yq
 

b1235468ae56dad11e794d8dc9d491d5abde1d19
Dennis Dyall <dennis.dyall@yubico.com>
2026-06-30 01:16:24 +0200

    refactor(fido2): keep only canonical puat status

--- src/Fido2/src/Ctap/CtapStatus.cs
+++ src/Fido2/src/Ctap/CtapStatus.cs
@@ -213,12 +213,6 @@ public enum CtapStatus : byte
     PuatRequired = 0x36,
 
     /// <summary>
-    /// A pinUvAuthToken is required for the selected operation.
-    /// </summary>
-    [Obsolete("Use PuatRequired instead.", false)]
-    PuvathRequired = PuatRequired,
-
-    /// <summary>
     /// PIN policy violation. Currently only enforces minimum length.
     /// </summary>
     PinPolicyViolation = 0x37,
